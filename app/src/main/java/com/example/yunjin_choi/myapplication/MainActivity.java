d9f2828bdc868970d90300f91a149f1ecb46cdd0
yunjin choi <zzdd1558@naver.com>
2018-07-21 17:07:02 +0900

    CollapsingToolbar

--- app/src/main/java/com/example/yunjin_choi/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yunjin_choi/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.yunjin_choi.myapplication;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -24,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar);
+
+        collapsingToolbarLayout.setTitle("My Toolbar Title");
+        collapsingToolbarLayout.setContentScrimColor(Color.GREEN);
 
         mCtx = this;
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
